4dd93d44967eff502fff966288f5757dbbcba301
edmundo papi <edmundopierre@yahoo.com>
2020-08-03 23:47:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student
   attr_accessor :name , :grade
   attr_reader :id
-  
+
   def initialize(name, grade, id =nil)
-    @id = id 
-    @name = name 
+    @id = id
+    @name = name
     @grade = grade
 
   end
